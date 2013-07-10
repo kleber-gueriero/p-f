2494edb9b5e1c09a2ab67845d14a3c226cfef9fc
Kleber Nascimento Gueriero <kleberng1990@gmail.com>
2013-07-10 15:53:18 -0300

    traduções e alteração para usar enviar email para pereiraefranco@hotmail.com

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
 class ContactMailer < ActionMailer::Base
-  default to: "kleberng1990@gmail.com", from: "kleberng1990@gmail.com"
+  default to: "kleberng1990@gmail.com", from: "contato@pereiraefranco.com.br"
   
   def contact_message(email_message)
     #@url  = "http://example.com/login"
     @email_message = email_message
     #mail options: :subject, :to, :from, :cc, :bcc, :reply_to, :date
-    mail(:from => @email_message.from, :subject => @email_message.subject)
+    mail(:subject => @email_message.subject)
   end
   
 end
